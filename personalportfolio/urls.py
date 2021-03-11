3cd59225b456301e2865e292c7d5250b07d5331a
tejaskerai <tejaskerai@gmail.com>
2021-03-11 17:43:38 +0000

    url patterns

--- personalportfolio/urls.py
+++ personalportfolio/urls.py
@@ -19,12 +19,17 @@ from django.conf.urls.static import static
 from django.conf import settings
 from personalportfolio import views
 
+from django.conf.urls import url
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('portfolio/', include('portfolio.urls')),
     path('interests/', include('interests.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
