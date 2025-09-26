f37fc4f2e58fde63c6241a296ecb9cb8b69184ab
Tejas Kerai <38558558+tejaskerai@users.noreply.github.com>
2025-09-26 19:21:32 +0100

    Move admin static files to staticfiles directory
    Reorganized static admin files by moving them from the 'static/admin' directory to 'staticfiles/admin', updating references in settings.py accordingly. Also fixed a typo in MEDIA_ROOT and removed obsolete or duplicate settings. This change supports proper static file collection and serving in production environments.

--- personalportfolio/settings.py
+++ personalportfolio/settings.py
@@ -122,16 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATIC_ROOT = BASE_DIR / 'static'
-
-STATICFILES_DIR = [
-    BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static', 
 ]
 
 MEDIA_URL = '/media/'
-
-MEDIA_ROUTE = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 try:
     from .local_settings import *
