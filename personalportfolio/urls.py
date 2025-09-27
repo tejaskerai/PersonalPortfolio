853060ca0fc90ac74221298fddb8116e1cc38c00
Tejas Kerai <38558558+tejaskerai@users.noreply.github.com>
2025-09-27 13:24:20 +0100

    update urls

--- personalportfolio/urls.py
+++ personalportfolio/urls.py
@@ -1,25 +1,14 @@
 """personalportfolio URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+The `urlpatterns` list routes URLs to views.
 """
 
 from django.contrib import admin
 from django.urls import path, include
+from personalportfolio import views
 from django.conf import settings
 from django.conf.urls.static import static
-from personalportfolio import views
-from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
@@ -28,9 +17,9 @@ urlpatterns = [
     path('education/', include('education.urls')),
     path('experience/', include('experience.urls')),
     path('interests/', include('interests.urls')),
-    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
-    path('static/<path:path>', serve, {'document_root': settings.STATIC_ROOT})]
+]
 
+# Only serve static/media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
